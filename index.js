a022a15d475f90e903d0644922b48fa44218f472
06000208 <52764066+06000208@users.noreply.github.com>
2022-02-01 15:35:52 -0600

    Update index.js to esm

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const fs = require("fs");
-const path = require("path");
-const process = require("process");
-const dotenv = require("dotenv");
-const Discord = require("discord.js");
-const log = require("./modules/log");
-const package = require("./package.json");
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { readFileSync, existsSync } from "node:fs";
+import process, { version as nodeVersion } from "node:process";
+import { version as discordVersion } from "discord.js";
+import dotenv from "dotenv";
+import { log } from "./modules/log.js";
+
+const moduleDirectory = dirname(fileURLToPath(import.meta.url));
+
+// Parse package.json
+const { name: projectName, version: projectVersion } = JSON.parse(readFileSync(join(moduleDirectory, "package.json")));
 
 // node.js process event listeners (if you can improve these, please contribute!)
 // https://nodejs.org/api/process.html (list is under Process Events)
@@ -19,23 +24,23 @@ process.on("exit", (code) => code === 0 ? log.info("Exiting peacefully") : log.w
 
 // node.js and discord.js version checks
 // version < minVersion
-const [nodeMajor, nodeMinor] = process.version.slice(1).split(".");
-const [discordMajor, discordMinor] = Discord.version.split(".");
-if (Number(nodeMajor) < 16 || Number(nodeMinor) < 6) {
-    log.fatal(`node.js v16.6+ is required, currently ${process.version}`);
+const [nodeMajor, nodeMinor] = nodeVersion.slice(1).split(".");
+const [discordMajor, discordMinor] = discordVersion.split(".");
+if (Number(nodeMajor) < 16 || Number(nodeMinor) < 9) {
+    log.fatal(`node.js v16.9+ is required, currently ${nodeVersion}`);
     process.exit(1);
-} else if (Number(discordMajor) < 13 || Number(discordMinor) < 3) {
-    log.fatal(`discord.js v13.3+ is required, currently v${Discord.version}`);
+} else if (Number(discordMajor) < 13 || Number(discordMinor) < 6) {
+    log.fatal(`discord.js v13.6+ is required, currently v${discordVersion}`);
     process.exit(1);
 } else {
-    log.info(`Starting ${package.name} v${package.version} using node.js ${process.version} and discord.js v${Discord.version} on ${process.platform}`);
+    log.info(`Starting ${projectName} v${projectVersion} using node.js ${nodeVersion} and discord.js v${discordVersion} on ${process.platform}`);
 }
 
+// Populate environment variables
 const envName = "";
 const envFile = envName + ".env";
-const envPath = path.join(__dirname, envFile);
-
-if (fs.existsSync(envPath)) {
+const envPath = join(moduleDirectory, envFile);
+if (existsSync(envPath)) {
     const result = dotenv.config({ path: envPath });
     if (result.parsed) log.info(`Loaded environment variables from "${envFile}":`, Object.keys(result.parsed));
 } else {
@@ -44,5 +49,7 @@ if (fs.existsSync(envPath)) {
 
 if (process.env.NODE_ENV === "development") log.info("Running in a development environment (NODE_ENV set to \"development\")");
 
-// Work in progress
-require("./bot");
+// Temporary
+(async function() {
+    await import("./bot.js");
+})();
