7a4eec56b1b67e930c471d2ddf47dc7acfc2b03a
06000208 <52764066+06000208@users.noreply.github.com>
2022-09-02 17:08:46 -0500

    Create deploy.js

---
+++ deploy.js
@@ -0,0 +1,4 @@
+// order of import statements matters, loaded asynchronously
+import "./modules/scripts/processEvents.js"; // Node.js process events script
+import "./modules/scripts/env.js"; // Populates environment variables script
+import "./modules/scripts/deployCommands.js"; // Deploy commands
