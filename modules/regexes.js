9da4876f9dd143119ae43ccb1e3dd90394210577
06000208 <52764066+06000208@users.noreply.github.com>
2022-09-03 00:07:08 -0500

    Remove redundant/broken regexes

--- modules/regexes.js
+++ modules/regexes.js
@@ -1,14 +1,16 @@
 /**
  * This module contains useful regular expressions
  *
- * Note that there are also useful regexes in discord.js accessible as static properties, specifically:
+ * Note that there are also useful regexes in discord.js:
  *
- * - [GuildTemplate.GUILD_TEMPLATES_PATTERN](https://discord.js.org/#/docs/main/stable/class/GuildTemplate?scrollTo=GUILD_TEMPLATES_PATTERN)
- * - [Invite.INVITES_PATTERN](https://discord.js.org/#/docs/main/stable/class/Invite?scrollTo=INVITES_PATTERN)
- * - [MessageMentions.EVERYONE_PATTERN](https://discord.js.org/#/docs/main/stable/class/MessageMentions?scrollTo=EVERYONE_PATTERN)
- * - [MessageMentions.USERS_PATTERN](https://discord.js.org/#/docs/main/stable/class/MessageMentions?scrollTo=USERS_PATTERN)
- * - [MessageMentions.ROLES_PATTERN](https://discord.js.org/#/docs/main/stable/class/MessageMentions?scrollTo=ROLES_PATTERN)
- * - [MessageMentions.CHANNELS_PATTERN](https://discord.js.org/#/docs/main/stable/class/MessageMentions?scrollTo=CHANNELS_PATTERN)
+ * - [GuildTemplate.GuildTemplatesPattern](https://discord.js.org/#/docs/discord.js/14.3.0/class/GuildTemplate?scrollTo=s-GuildTemplatesPattern)
+ * - [Invite.InvitesPattern](https://discord.js.org/#/docs/discord.js/14.3.0/class/Invite?scrollTo=s-InvitesPattern)
+ * - [MessageMentions.EveryonePattern](https://discord.js.org/#/docs/discord.js/14.3.0/class/MessageMentions?scrollTo=s-EveryonePattern)
+ * - [MessageMentions.UsersPattern](https://discord.js.org/#/docs/discord.js/14.3.0/class/MessageMentions?scrollTo=s-UsersPattern)
+ * - [MessageMentions.RolesPattern](https://discord.js.org/#/docs/discord.js/14.3.0/class/MessageMentions?scrollTo=s-RolesPattern)
+ * - [MessageMentions.ChannelsPattern](https://discord.js.org/#/docs/discord.js/14.3.0/class/MessageMentions?scrollTo=s-ChannelsPattern)
+ *
+ * As well as sapphiredev's [discord-utilities](https://www.sapphirejs.dev/docs/Documentation/api-utilities/modules/discord_utilities_src)
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#creating_a_regular_expression
  * @module regexes
  */
@@ -27,17 +29,3 @@ module.exports.numeric = /^\d+$/;
  * Matches only characters in the ranges A-Z, a-z, and any digit character (0-9) from the beginning to the end of the string.
  */
 module.exports.alphanumeric = /^[A-Za-z\d]+$/;
-
-/**
- * Matches the format for a discord authentication token from the beginning to the end of the string.
- * @todo Is the number of characters per part guaranteed to be 24,6,27 or not?
- * @todo This isn't affected by [this](https://stackoverflow.com/a/1520853), right?
- */
-module.exports.discordToken = /^[\w]{24}\.[\w-]{6}\.[\w-]{27}$/;
-
-/**
- * Matches discord message URLs with capturing groups for the three ids (guild, channel, and message)
- *
- * The latter two ids may be omitted (ie. `https://discord.com/channels/1` is valid), as some purposes only require certain ids
- */
-module.exports.messageURL = /^https?:\/\/discord(?:app)?\.com\/channels\/(\d+)\/?(\d+)?\/?(\d+)?\/?$/i;
