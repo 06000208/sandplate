895de93e29b695764309db13dc4dbbb6caf51e95
amber <a06000208@protonmail.com>
2022-10-27 19:53:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("./modules/scripts/processEvents.js"); // Node.js process events
-require("./modules/scripts/versionCheck.js"); // Version checks
+require("./modules/scripts/versionCheck.js"); // Node.js version check
 require("./modules/scripts/env.js"); // Populates environment variables
 require("./modules/scripts/startupMessage.js"); // Startup message
 require("./modules/discord.js"); // Start discord bot
